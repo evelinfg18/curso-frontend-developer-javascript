b22e5a8ae90e8f75394b92dbed7553bebfc11a81
evelinfg18 <evelinfg18@hotmail.com>
2022-09-24 00:53:48 -0500

    fusion del menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
+menuHamIcon.addEventListener('click', toogleMobileMenu);
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu() {
+    menuMobile.classList.toggle('inactive');
 }
